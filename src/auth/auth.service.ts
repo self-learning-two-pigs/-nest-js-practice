fb697237423c3bf4a5889464a82af673f0e11c1b
pingyang <pingyang@thoughtworks.com>
2023-12-12 15:48:59 +0800

    return jwt token when login success

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { LoginDto } from './auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login({ name, password }: LoginDto) {
     const user = await this.userService.getByName(name);
@@ -15,6 +19,15 @@ export class AuthService {
     if (!isSame) {
       throw new UnauthorizedException('password is not match');
     }
-    return user;
+    const { id } = user;
+    const token = this.jwtService.sign({
+      id,
+      name,
+    });
+    return {
+      id,
+      name,
+      token,
+    };
   }
 }
